ade6ad8adcea511d5b7479563ee083c55ba7e7af
Neil Lancaster <nlanky@hotmail.co.uk>
2021-11-04 23:03:14 +0000

    Remove filtering of imports

--- src/constants.ts
+++ src/constants.ts
@@ -28,43 +28,6 @@ export const NOTES = [
 export type Note = typeof NOTES[number];
 
 /*
-  A mapping of which note samples are acceptable for use on piano keys.
-  Any that aren't going to be tied to a piano key are added to 'other'
-  which are played back using BonusButton.
-*/
-export const THEME_TO_VALID_INDICES: Record<
-  string,
-  Record<Note, number[]>
-> = {
-  sharky: {
-    C4: [0],
-    Csharp4: [0],
-    D4: [3],
-    Dsharp4: [0, 1, 2],
-    E4: [1, 7, 8],
-    F4: [4, 5],
-    Fsharp4: [1, 2, 3, 4, 11, 13],
-    G4: [1],
-    Gsharp4: [5, 7],
-    A4: [2, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 18, 19],
-    Asharp4: [1],
-    B4: [2, 4, 5, 9],
-    C5: [1],
-    Csharp5: [0, 2, 3],
-    D5: [0, 1, 3],
-    Dsharp5: [0, 2, 5],
-    E5: [2],
-    F5: [2],
-    Fsharp5: [2],
-    G5: [2],
-    Gsharp5: [2],
-    A5: [2],
-    Asharp5: [2],
-    B5: [2],
-  },
-};
-
-/*
   Using a map to preserve order of notes.
   This makes it simple to loop through and render them in order.
 */
